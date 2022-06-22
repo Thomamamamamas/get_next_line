39c1dc8bf22dfcc8c17b1c50db920d679e356d97
Thomas Casale <tcasale@student.42nice.fr>
2022-06-22 16:37:06 +0200

    ready to pass Moulinette

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: tcasale <tcasale@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 12:55:33 by tcasale           #+#    #+#             */
-/*   Updated: 2022/03/20 12:55:35 by tcasale          ###   ########.fr       */
+/*   Updated: 2022/06/22 16:34:29 by tcasale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_BONUS_H
@@ -19,8 +19,8 @@
 char	*get_next_line(int fd);
 char	*trimline(char *save);
 char	*restline(char *save);
-char	*ft_strchr(char *s, int c);
-size_t	ft_strlen(char *s);
-char	*ft_strjoin(char *s1, char *s2);
+char	*gnl_ft_strchr(char *s, int c);
+size_t	gnl_ft_strlen(char *s);
+char	*gnl_ft_strjoin(char *s1, char *s2);
 
 #endif
